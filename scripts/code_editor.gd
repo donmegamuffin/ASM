216c75558d7647a22d82fac805977a5028db376f
Zachary Humphreys <wise742@wise742.broadband>
2026-03-18 22:22:16 +0000

    Unfucked code editor

--- scripts/code_editor.gd
+++ scripts/code_editor.gd
@@ -2,7 +2,7 @@ extends Control
 
 func _ready() -> void:
 	print(ASMInstructions.EInstruction.MAX)
-	var code: ASMCompiler.Program = ASMCompiler.jit(%CodeEdit.text)
+	var code: ASMCompiler.Program = ASMCompiler.compile(%CodeEdit.text)
 	print(type_string(typeof(code)))
 	for line in code.instructions:
 		print(line)
